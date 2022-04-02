22aa4fe63eb044edfc938efd4f06cad03853069b
FelipeRon <37913814+FelipeRon@users.noreply.github.com>
2022-04-01 21:02:06 -0500

    Add files via upload

---
+++ cronometro/src/cronometro/Cronometro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cronometro;
+
+/**
+ *
+ * @author nel_g
+ */
+public class Cronometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmCronometro frmcronometro = new FrmCronometro();
+        frmcronometro.setVisible(true);
+    }
+    
+}
